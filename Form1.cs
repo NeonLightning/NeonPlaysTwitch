011c99609830f50c65096604a3db2e86c42094d8
NeonLightning <neonlightning@gmail.com>
2022-10-14 12:45:15 -0400

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+using System.Net;
+using System.Collections.Specialized;
+
+namespace NeonPlaysTwitch
+{
+    public partial class Form1 : Form
+    {
+
+
+        [DllImport("user32.dll")]
+        static extern IntPtr GetForegroundWindow();
+        [DllImport("user32.dll")]
+        static extern int GetWindowText(IntPtr hWnd, StringBuilder text, int count);
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void GetActiveWindow()
+        {
+            const int nChars = 256;
+            IntPtr handle;
+            StringBuilder Buff = new StringBuilder(nChars);
+            handle = GetForegroundWindow();
+            if (GetWindowText(handle, Buff, nChars) > 0)
+            {
+                string Currentwindow = Buff.ToString();
+            }
+        }
+
+        [DllImport("user32.dll")]
+        internal static extern IntPtr SetForegroundWindow(IntPtr hWnd);
+
+        [DllImport("user32.dll")]
+        internal static extern bool ShowWindow(IntPtr hWnd, int nCmdShow); //ShowWindow needs an IntPtr
+        string Currentwindow;
+        private static void FocusVBA()
+        {
+            IntPtr hWnd; //change this to IntPtr
+            Process[] processRunning = Process.GetProcesses();
+            foreach (Process pr in processRunning)
+            {
+                if (pr.ProcessName == "VisualBoyAdvance")
+                {
+                    hWnd = pr.MainWindowHandle; //use it as IntPtr not int
+                    ShowWindow(hWnd, 3);
+                    SetForegroundWindow(hWnd); //set to topmost
+                }
+            }
+        }
+        private static void FocusCurrent(string Currentwindow)
+        {
+            IntPtr hWnd; //change this to IntPtr
+            Process[] processRunning = Process.GetProcesses();
+            foreach (Process pr in processRunning)
+            {
+                if (pr.ProcessName == Currentwindow)
+                {
+                    hWnd = pr.MainWindowHandle; //use it as IntPtr not int
+                    ShowWindow(hWnd, 3);
+                    SetForegroundWindow(hWnd); //set to topmost
+                }
+            }
+        }
+
+        public void button7_Click(object sender, EventArgs e)
+        {
+            // Send the enter key; since the tab stop of Button1 is 0, this
+            // will trigger the click event.
+            GetActiveWindow();
+            FocusVBA();
+            SendKeys.Send("7");
+            FocusCurrent("Currentwindow");
+        }
+    }
+}
